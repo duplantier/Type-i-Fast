3b584aa1709ab4728f62187960a41b6d690a2b78
Huseyin Karatas <huseyin.karatas@metu.edu.tr>
2023-11-22 18:29:17 +0300

    Live character control logic

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 // How to get quotes: api.quotable.io/random
-const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteInput');
+
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('');
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index];
+        if (character == null) {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.remove('incorrect');
+        }
+        else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        } else {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.add('incorrect');
+        }
+
+
+    })
+});
 
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
@@ -9,9 +32,15 @@ function getRandomQuote() {
         .then(data => data.content)
 }
 
-async function getNextQuote() {
-    const quote = await getRandomQuote()
-    console.log(quote);
+async function renderNewQuote() {
+    const quote = await getRandomQuote();
+    quoteDisplayElement.innerHTML = '';
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        quoteDisplayElement.appendChild(characterSpan);
+    })
+    quoteInputElement.value = null;
 }
 
-getNextQuote()
+renderNewQuote()
