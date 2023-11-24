50b55bb3c55437f48368e645af1966bcbf03a66d
Huseyin Karatas <huseyin.karatas@metu.edu.tr>
2023-11-25 00:08:45 +0300

    First demo of real counter and controls. Still  improving

--- script.js
+++ script.js
@@ -2,45 +2,20 @@
 const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
+
 const timerElement = document.getElementById('timer');
-let correct = true;
+const timeSelectionElement = document.getElementById('timeSelection');
+const submitTimeSelectionButton = document.getElementById('submitTimeSelection');
+const quoteCounterElement = document.getElementById('quoteCounterElement');
+const correctCharacterCounterElement = document.getElementById('correctCharacterCounterElement');
+const incorrectCharacterCounterElement = document.getElementById('incorrectCharacterCounterElement');
 
-let roundTime;
-function getTimerTime() {
-    return Math.floor((new Date() + roundTime) / 1000);
-}
-
-function startTimer() {
-    timerElement.innerText = 0;
-    roundTime = new Date();
-    setInterval(() => {
-        timerElement.innerText = getTimerTime();
-    }, -1000)
-}
-
-function getRandomQuote() {
-    let content = fetch(RANDOM_QUOTE_API_URL)
-        // Take response and return response.json() - json format of the response
-        .then(response => response.json())
-        // Take data and return data.content
-        .then(data => data.content)
-
-    return content;
-}
-
-async function renderNewQuote() {
-    const quote = await getRandomQuote();
-    quoteDisplayElement.innerHTML = '';
-    quote.split('').forEach(character => {
-        const characterSpan = document.createElement('span');
-        characterSpan.innerText = character;
-        quoteDisplayElement.appendChild(characterSpan);
-    })
-    quoteInputElement.value = null;
-    startTimer();
-}
+let newQuoteCounter;
+let correctCharacterCounter;
+let incorrectCharacterCounter;
 
 
+correct = true;
 function startRound() {
     quoteInputElement.addEventListener('input', () => {
         const arrayQuote = quoteDisplayElement.querySelectorAll('span')
@@ -55,22 +30,69 @@ function startRound() {
             else if (character === characterSpan.innerText) {
                 characterSpan.classList.add('correct');
                 characterSpan.classList.remove('incorrect');
+                correctCharacterCounter += 1;
             } else {
                 characterSpan.classList.remove('correct');
                 characterSpan.classList.add('incorrect');
+                incorrectCharacterCounter += 1;
                 correct = false;
             }
 
 
         })
 
-        if (correct) renderNewQuote();
+        if (correct) {
+            newQuoteCounter = 0;
+            renderNewQuote();
+            newQuoteCounter += 1;
+        }
     });
     renderNewQuote()
 }
 
+submitTimeSelectionButton.addEventListener('click', () => {
+    let selectedTime = timeSelectionElement.value;
+    timerElement.innerText = selectedTime;
+
+    intervalId = setInterval(() => {
+        timerElement.innerText = selectedTime--;
+        if (selectedTime == 0) {
+            clearInterval(intervalId);
+            quoteInputElement.disabled = true;
+            timerElement.innerText = "Time is up!";
+            quoteCounterElement.innerText = newQuoteCounter;
+            correctCharacterCounterElement.innerText = correctCharacterCounter;
+            incorrectCharacterCounterElement.innerText = incorrectCharacterCounter;
+        }
+    }, 1000);
+    startRound();
+});
+
+function getRandomQuote() {
+    let content = fetch(RANDOM_QUOTE_API_URL)
+        // Take response and return response.json() - json format of the response
+        .then(response => response.json())
+        // Take data and return data.content
+        .then(data => data.content)
+    return content;
+}
+
+async function renderNewQuote() {
+    const quote = await getRandomQuote();
+    quoteDisplayElement.innerHTML = '';
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        quoteDisplayElement.appendChild(characterSpan);
+    })
+    quoteInputElement.value = null;
+}
 
 
+
+
+// Doğru yapılan quote'ları kaydet
+// En sonda bir analiz göster
 
 
 
