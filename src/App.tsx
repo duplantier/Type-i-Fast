76c75c8b6f62a9a4a3fd8288b8743d7e1166ccfc
Huseyin Karatas <huseyin.karatas@metu.edu.tr>
2023-12-14 21:41:01 +0300

    Build: Results component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { faker } from "@faker-js/faker";
 import RestartButton from "./components/RestartButton";
+import Results from "./components/Results";
+
 const words = faker.random.words(10);
 
 function App() {
@@ -10,6 +12,12 @@ function App() {
       <RestartButton
         className={"mx-auto mt-10 text-slate-500"}
         onRestart={() => null}
+      />
+      <Results
+        className="mt-10"
+        errors={10}
+        accuracyPercentage={90}
+        total={200}
       />
     </>
   );
